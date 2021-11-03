7bb5a16bf082a34c447a19cdb8200fa30c2bf791
ron4mac <ron@rjconline.net>
2021-11-03 13:51:49 +0000

    improved installer script

--- script.php
+++ script.php
@@ -7,27 +7,42 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\InstallerScript;
+use Joomla\CMS\Log\Log;
 
-class com_usernotesInstallerScript
+class com_usernotesInstallerScript extends InstallerScript
 {
-	function install ($parent)
+	protected $minimumJoomla = '3.8';
+	protected $com_name = 'com_usernotes';
+
+	public function install ($parent)
 	{
-		$parent->getParent()->setRedirectURL('index.php?option=com_usernotes');
+		$parent->getParent()->setRedirectURL('index.php?option='.$this->com_name);
 	}
 
 
-	function uninstall ($parent)
+	public function uninstall ($parent)
 	{
 	}
 
 
-	function update ($parent)
+	public function update ($parent)
 	{
 	}
 
 
-	function preflight ($type, $parent)
+	public function preflight ($type, $parent)
 	{
+		// give the parent first shot
+		if (parent::preflight($type, $parent) === false) return false;
+
+		// ensure that SQLite is active in joomla
+		$dbs = JDatabaseDriver::getConnectors();
+		if (!in_array('sqlite', $dbs) && !in_array('Sqlite', $dbs)) {
+			Log::add('Joomla support for SQLite(3) is required for this component.', Log::WARNING, 'jerror');
+			return false;
+		}
+		// get the version number being installed/updated
 		if (method_exists($parent,'getManifest')) {
 			$this->release = $parent->getManifest()->version;
 		} else {
@@ -35,27 +50,27 @@ class com_usernotesInstallerScript
 		}
 	}
 
-	function postflight ($type, $parent)
+	public function postflight ($type, $parent)
 	{
 		$params['version'] = $this->release;
-		$this->setParams($params, true);
+		$this->mySetParams($params, true);
 		if ($type == 'install') {
 			$params['storQuota'] = 268435456;
 			$params['maxUpload'] = 4194304;
-			$params['hide_secure'] = 1;
+			$params['hide_secure'] = true;
 			$params['standard_skin'] = '';
-			$params['show_version'] = 0;
-			$this->setParams($params);
+			$params['show_version'] = false;
+			$this->mySetParams($params);
 		}
 	}
 
 
-	private function setParams ($param_array, $replace=false)
+	private function mySetParams ($param_array=[], $replace=false)
 	{
 		if (count($param_array) > 0) {
 			// read the existing component value(s)
 			$db = Factory::getDbo();
-			$db->setQuery('SELECT params FROM #__extensions WHERE name = "com_usernotes"');
+			$db->setQuery('SELECT params FROM #__extensions WHERE name = "'.$this->com_name.'"');
 			$params = json_decode($db->loadResult(), true);
 			// add the new variable(s) to the existing one(s), replacing existing only if requested
 			foreach ($param_array as $name => $value) {
@@ -64,7 +79,7 @@ class com_usernotesInstallerScript
 			}
 			// store the combined new and existing values back as a JSON string
 			$paramsString = json_encode($params);
-			$db->setQuery('UPDATE #__extensions SET params = ' . $db->quote($paramsString) . ' WHERE name = "com_usernotes"');
+			$db->setQuery('UPDATE #__extensions SET params = ' . $db->quote($paramsString) . ' WHERE name = "'.$this->com_name.'"');
 			$db->execute();
 		}
 	}
